c409670378fe6e2468f18d5194f935a626ef7a27
Kenton Lam <kenton_lam@outlook.com>
2019-09-10 21:54:10 +1000

    optimising away matrix products

--- householder.py
+++ householder.py
@@ -32,36 +32,56 @@ def householder(A) -> Tuple[sp.matrix, sp.matrix]:
 
     Returns (Q, R) such that A = QR, Q is orthogonal and R is triangular.
     '''
-    Q_full = sps.identity(A.shape[0])
+    # Q_full = sps.identity(A.shape[0], format='csr')
     start = time.time()
+
+    A_full = sps.lil_matrix(A.shape)
+    A_sub = A.copy()
+
+    Q_full = sps.identity(A.shape[0], format='lil')
+    Q_sub = sps.csr_matrix(Q_full)
+
     for i in range(min(A.shape)): # iterate over smaller dimension of A
-        _A = A[i:,i:] # submatrix of A left to triangularise
+        # _A = A[i:,i:] # submatrix of A left to triangularise
         # print(_A)
         if i % 100 == 0:
             print(i, time.time() - start)
             start = time.time()
         # print(i)
-        v = (_A[:,0]) # leftmost vector of A matrix
-        e_i = sp.zeros((v.shape[0], 1))
+        v = A_sub[:, 0] # leftmost vector of A matrix
+        e_i = sps.dok_matrix((v.shape[0], 1))
         e_i[0] = 1 # vector with 1 in the first position.
-        e_i = sps.csr_matrix(e_i)
+        e_i = sps.csc_matrix(e_i)
         
-        v_max = v.min() if -v.min() > v.max() else v.max()
+        v_max = 1 #v.min() if -v.min() > v.max() else v.max()
         u = v + sign(v_max) * spsla.norm(v) * e_i # compute u vector for P
         u = u / spsla.norm(u) # normalise
         _P = sps.identity(u.shape[0], format='csr') - 2 * (u @ u.T) # compute sub _P
 
+        # _Q = sps.csr_matrix(Q_full[i:, i:])
+        
         # embed this submatrix _P into the full size P
-        P = sps.block_diag((sps.identity(i, format='dia'), _P))
+        # P = sps.block_diag((sps.identity(i), _P), format='csc')
 
-        Q_full = P @ Q_full # accumulated orthogonal Q
-        A = P @ A # applying to A
+        Q_sub = _P @ Q_sub
+        Q_full[i,i:] = Q_sub[0,:]
+        Q_full[i:,i] = Q_sub[:,0]
+
+        Q_sub = Q_sub[1:, 1:]
+
+        # Q_full[i:,i:] = _P @ Q_full[i:,i:] # accumulated orthogonal Q
+        # A[i:,i:] = _P @ A[i:,i:] # applying to A
+        A_sub = _P @ A_sub
+        A_full[i,i:] = A_sub[0,:]
+        A_full[i:,i] = A_sub[:,0]
+
+        A_sub = A_sub[1:,1:]
     
     # A = QR
     # Q_full is currently the inverse because it is applied to A, to R.
     # thus, Q = Q_full^T.
     # note that A has been reduced to R by applying the P's.
-    return (Q_full.transpose(), A)
+    return (Q_full.transpose(), sps.triu(A_full))
     
 
 if __name__ == "__main__":
@@ -76,14 +96,16 @@ if __name__ == "__main__":
         [1, 2, 3, 4],
         [5, 6, 7, 8]
     ])
+    A = sps.csr_matrix(A)
 
     L = lcd(0.1, 0.1, 100)
-    L = sps.csr_matrix(L)
+    L = sps.csc_matrix(L)
     
-    # A = sps.csr_matrix(A)
     A = L
     Q, R = householder(A)
-    print('QR - A:', spsla.norm(Q @ R - A))
-    print('QQ^T - I:', spsla.norm(Q @ Q.transpose() - sps.identity(Q.shape[0])))
-    print('Q^TQ - I:', spsla.norm(Q.transpose() @ Q - sps.identity(Q.shape[0])))
+    print(Q.toarray())
+    print(R.toarray())
+    print('QR - A:', spsla.norm(Q @ R - A, sp.inf))
+    print('QQ^T - I:', spsla.norm(Q @ Q.transpose() - sps.identity(Q.shape[0]), sp.inf))
+    print('Q^TQ - I:', spsla.norm(Q.transpose() @ Q - sps.identity(Q.shape[0]), sp.inf))
     
