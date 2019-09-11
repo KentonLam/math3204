32bab75b34f985bb7700f368ee54f8699ee4a7d1
Kenton Lam <kenton_lam@outlook.com>
2019-09-11 16:40:47 +1000

    i've done something wrong

--- pcg.py
+++ pcg.py
@@ -5,10 +5,12 @@ import scipy.sparse.linalg as spsla
 
 from householder import lcd
 
-def cg(A, b, x_0, P=None):
+def pcg(A, b, x_0, P=None):
     if P is None:
         P = sp.identity(A.shape[0])
-    r_0 = b - A @ x_0 
+    P_inv = spla.inv(P)
+    r_0 = b - A @ x_0
+    A = P_inv @ A
 
     r_prev = r = r_0
     x = x_0
@@ -18,12 +20,13 @@ def cg(A, b, x_0, P=None):
     while True:
         k += 1
         Ap = A @ p
-        alpha = ((r.T @ r) / (p.T @ Ap)).item()
+        alpha = ((r.T @ P @ r) / (p.T @ P @ Ap)).item()
         x = x + alpha*p # x_k initially stores x_{k-1}
 
         r_prev = r
         r = r - alpha * Ap
-        beta = ((r.T @ r) / (r_prev.T @ r_prev)).item()
+        # r = P_inv @ r
+        beta = ((r.T @ P @ r) / (r_prev.T @ P @ r_prev)).item()
         p = r + beta * p 
         print(k, spla.norm(r))
 
@@ -40,4 +43,4 @@ if __name__ == "__main__":
     A = lcd(0, 0, N).todense()
     b = sp.ones((N**2, 1))
     x_0 = sp.zeros((N**2, 1))
-    cg(A, b, x_0)
+    pcg(A, b, x_0, sp.diag(sp.diag(A)))
